0a5090268c90bed25af58a85946937ee172bcad5
vagrant <vagrant@ironhack>
2014-02-21 16:52:53 +0000

    first three tests

---
+++ TDD_kata.js
@@ -0,0 +1,65 @@
+/* TO-DO LIST
+
+* ""   --> 0
+* "6"  --> 6
+* "4,3"--> 7
+* "a"  --> 0
+* "aa" --> 0
+* "a45"--> 0
+
+*/
+
+chai.should(); // invoking this function creates a "should" object on every object
+context = describe;
+
+function sumNum (string) {
+
+	var number = parseInt(string);
+
+	if (!isNaN(number)){
+		return number;
+	}else{
+
+		var result = 0;
+
+		var listOfItems = string.split(",");
+		for (var i = 0; i < listOfItems.length; i++) {
+			if(isNaN(parseInt(istOfItems[i])){
+				return 0;
+			}else{
+				result += parseInt(listOfItems[i]);
+			}
+			return result;
+		};
+	
+	}
+};
+
+describe ("sumNum: ", function(){
+
+  it("empty string returns 0", function(){
+
+  	var result = sumNum("");
+    expect(result).toEqual(0);      
+
+  });
+
+    it("number returns 0", function(){
+
+  	var result = sumNum("6");
+    expect(result).toEqual(6);      
+
+  });
+
+	it("one letter character string returns 0", function(){
+
+	  	var result = sumNum("4,3");
+	    expect(result).toEqual(7);      
+
+	  });
+
+
+  
+
+
+});
